9717173e7b3a4417feaa12f561e2dfe30619fba2
Pulkit Agarwal <pulkitagrawal005@gmail.com>
2024-10-17 23:17:16 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Pulkit",
+    author_email="pulkitagarwal2407@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
